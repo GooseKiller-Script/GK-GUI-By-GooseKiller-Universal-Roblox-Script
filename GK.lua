e39888606a881335eebeac9b0932fc0770733ac4
GooseKiller <goose9876543210@gmail.com>
2025-06-08 20:17:59 +0300

    Update GK.lua

--- GK.lua
+++ GK.lua
@@ -1139,6 +1139,16 @@ CreateButton("Auto Kill", function(btn)
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/GooseKiller-Script/GK-GUI-By-GooseKiller-Universal-Roblox-Script/main/AutoKill.lua"))()
 	end
 end)
+CreateButton("TriggerBot", function(btn)
+	pcall(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/GooseKiller-Script/GK-GUI-By-GooseKiller-Universal-Roblox-Script/main/TriggerBot.lua"))()
+	end
+end)
+CreateButton("Slient Aim", function(btn)
+	pcall(function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/GooseKiller-Script/GK-GUI-By-GooseKiller-Universal-Roblox-Script/main/SlientAim.lua"))()
+	end
+end)
 RunService.Heartbeat:Connect(function()
     if Functions.ESP then
         UpdateESP() 
