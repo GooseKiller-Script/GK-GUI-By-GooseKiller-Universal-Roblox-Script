c3639c90c69fb7c1a3cb2951f0c58bfaa5ba5422
GooseKiller <goose9876543210@gmail.com>
2025-06-10 21:14:41 +0300

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -109,6 +109,12 @@ createButton("TriggerBot", function()
         end)
 end)
 
+createButton("Heal-Tools", function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/GooseKiller-Script/GK-GUI-By-GooseKiller-Universal-Roblox-Script/main/HealTools.lua"))()
+        end)
+end)
+
 local closeButton = Instance.new("TextButton")
 closeButton.Size = UDim2.new(0, 30, 0, 30)
 closeButton.Position = UDim2.new(1, -35, 0, 5)
